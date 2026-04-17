f71cb03f68671a5a8b4d26c96ac94fb488e1221b
nico25m <nickmelzi105@gmail.com>
2026-04-17 19:13:43 +0200

    .

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://includo-frontend-2mev89ef9-nico25ms-projects.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
